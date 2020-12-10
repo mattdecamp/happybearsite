2345dd5259e9601b0ab173d716b5a9c7dc59e16d
Matt DeCamp <matthewdecamp@gmail.com>
2020-12-09 20:35:11 -0500

    moved favicons

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,14 @@ module.exports = (config) => {
   config.addPassthroughCopy("./src/images/");
   config.addPassthroughCopy({ "./src/_includes/css": "/css" });
   config.addPassthroughCopy("./src/fonts/");
+  // Transforms
+  const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+  // Create a helpful production flag
+  const isProduction = process.env.NODE_ENV === "production";
+  // Only minify HTML if we are in production because it slows builds _right_ down
+  if (isProduction) {
+    config.addTransform("htmlmin", htmlMinTransform);
+  }
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
   return {
